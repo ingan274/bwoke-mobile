2bd416f91ec8193849e2af4f3cada7c15214ddfb
ingan <isabelngan27@gmail.com>
2019-12-04 21:23:10 -0800

    adding all the stuff

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,13 +7,15 @@ import {
     StyleSheet,
     View,
     TouchableWithoutFeedback,
-    AsyncStorage,
     Keyboard,
+    AsyncStorage,
+    Text
 } from 'react-native';
 
 import styled from "styled-components/native";
 import Background from "../assets/images/Background.gif";
 import LoginText from '../components/LoginSignUpText'
+import color from '../constants/Colors'
 
 export default class LoginScreen extends Component {
 
@@ -31,6 +33,7 @@ export default class LoginScreen extends Component {
     }
 
     enterApp = () => {
+      console.log("pressed")
         const login = {
             username: this.state.username,
             password: this.state.password,
@@ -47,7 +50,7 @@ export default class LoginScreen extends Component {
             .then((response) => {
                 if (response.error === "true") {
                     this.setState({ error: true });
-                } else {
+                } else if (response.error === "false") {
                     let username = this.state.username
                     this.handleLocalStorageUsername(username)
 
@@ -74,7 +77,7 @@ export default class LoginScreen extends Component {
 
     showError = () => {
         if (this.state.error) {
-            return <Text style={style.error}>The username and password you entered do not match.</Text>
+            return <Text style={styles.error}>The username and password you entered do not match.</Text>
         }
     };
 
@@ -96,7 +99,7 @@ export default class LoginScreen extends Component {
                         <ButtonWrapper>
                             <Fragment>
                                 {/* <Button title="Create Account" /> */}
-                                <Button transparent title="Login" />
+                                <Button transparent title="Login" onClick = {this.enterApp} />
                             </Fragment>
                         </ButtonWrapper>
                     </ImageBackground>
@@ -115,16 +118,6 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#000',
     },
-    developmentModeText: {
-        marginBottom: 20,
-        color: 'rgba(0,0,0,0.4)',
-        fontSize: 14,
-        lineHeight: 19,
-        textAlign: 'center',
-    },
-    contentContainer: {
-        paddingTop: 30,
-    },
     welcomeContainer: {
         alignItems: 'center',
         marginTop: 30,
@@ -137,66 +130,9 @@ const styles = StyleSheet.create({
         marginTop: 50,
         marginLeft: -10,
     },
-    getStartedContainer: {
-        alignItems: 'center',
-        marginHorizontal: 50,
-    },
-    WelcomeScreenFilename: {
-        marginVertical: 7,
-    },
-    codeHighlightText: {
-        color: 'rgba(96,100,109, 0.8)',
-    },
-    codeHighlightContainer: {
-        backgroundColor: 'rgba(0,0,0,0.05)',
-        borderRadius: 3,
-        paddingHorizontal: 4,
-    },
-    getStartedText: {
-        fontSize: 17,
-        color: 'rgba(96,100,109, 1)',
-        lineHeight: 24,
-        textAlign: 'center',
-    },
-    tabBarInfoContainer: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        ...Platform.select({
-            ios: {
-                shadowColor: 'black',
-                shadowOffset: { width: 0, height: -3 },
-                shadowOpacity: 0.1,
-                shadowRadius: 3,
-            },
-            android: {
-                elevation: 20,
-            },
-        }),
-        alignItems: 'center',
-        backgroundColor: '#fbfbfb',
-        paddingVertical: 20,
-    },
-    tabBarInfoText: {
-        fontSize: 17,
-        color: 'rgba(96,100,109, 1)',
-        textAlign: 'center',
-    },
-    navigationFilename: {
-        marginTop: 5,
-    },
-    helpContainer: {
-        marginTop: 15,
-        alignItems: 'center',
-    },
-    helpLink: {
-        paddingVertical: 15,
-    },
-    helpLinkText: {
-        fontSize: 14,
-        color: '#2e78b7',
-    },
+    error: {
+      color: color.red
+    }
 });
 
 // export const Wrapper = styled.View`
@@ -240,7 +176,7 @@ const StyledButton = styled.TouchableOpacity`
    margin-bottom:20px;
    border-radius:24px
   `;
-StyledTitle = styled.Text`
+const StyledTitle = styled.Text`
     text-transform: uppercase;
     text-align: center;
     font-weight: bold;
