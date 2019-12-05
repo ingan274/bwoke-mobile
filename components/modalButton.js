87d19a51b21a7b1600d7ef17bffdd7561eae7ce8
ingan <isabelngan27@gmail.com>
2019-12-04 19:05:08 -0800

    editing modal button and giving it props

--- components/modalButton.js
+++ components/modalButton.js
@@ -2,36 +2,29 @@ import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
-import AddEventModal from '../components/addEventModal'
 
 
-export default class ModalButton extends Component {
+export default function ModalButton(props) {
 
-    setModalVisible = (visible) => {
-        this.setState({ modalVisible: visible });
-    }
-
-    render() {
-        return (
-            <View style={{ flex: 1, backgroundColor: '#f3f3f3' }}>
-                {/* Rest of the app comes ABOVE the action button component !*/}
-                <ActionButton buttonColor="rgba(231,76,60,1)">
-                    <ActionButton.Item buttonColor='#9b59b6' title="Add Event" onPress={() => {
-                        AddEventModal
-                    }
-                    }>
-                        <Icon name="md-create" style={styles.actionButtonIcon} />
-                    </ActionButton.Item>
-                    {/* <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+    return (
+        <View style={{ flex: 1, backgroundColor: '#f3f3f3' }}>
+            {/* Rest of the app comes ABOVE the action button component !*/}
+            <ActionButton buttonColor="rgba(231,76,60,1)">
+                <ActionButton.Item buttonColor='#9b59b6' title="Add Event" onPress={() => {
+                    props.onPress
+                }
+                }>
+                    <Icon name="md-create" style={styles.actionButtonIcon} />
+                </ActionButton.Item>
+                {/* <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
             <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
           </ActionButton.Item>
           <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
             <Icon name="md-done-all" style={styles.actionButtonIcon} />
           </ActionButton.Item> */}
-                </ActionButton>
-            </View>
-        );
-    }
+            </ActionButton>
+        </View>
+    );
 
 }
 
--- screens/feedScreen.js
+++ screens/feedScreen.js
@@ -1,5 +1,4 @@
 import React, { PureComponent, Component, Fragment } from 'react';
-import AddEventModal from '../components/addEventModal'
 import { Ionicons } from '@expo/vector-icons';
 import color from '../constants/Colors'
 window.navigator.userAgent = 'ReactNative';
@@ -15,9 +14,9 @@ import {
     Navigator,
     PropTypes,
     Image,
-    ImageBackground,
-    TouchableOpacity,
-    ActivityIndicator,
+    Modal,
+    TouchableHighlight,
+    Alert,
     TouchableWithoutFeedback,
     Keyboard,
 
@@ -27,6 +26,11 @@ import {
 export default class EventFeed extends Component {
     state = {
         modalVisible: false,
+        nametitle: '',
+        date: '',
+        name: '',
+        description: '',
+        error: false,
     };
 
     componentDidMount = () => {
@@ -37,6 +41,43 @@ export default class EventFeed extends Component {
         this.setState({ modalVisible: visible });
     }
 
+    handleSubmit = () => {
+        let title = this.state.title;
+        let date = this.state.date;
+        let name = this.state.name;
+        let description = this.state.description;
+
+        let event = {
+            title: title,
+            date: date,
+            name: name,
+            description: description
+        }
+
+        if (title && date && name && description) {
+            fetch(`https://bwoke.herokuapp.com/events`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(event)
+            }).then((data) => {
+                console.log("Posted")
+            })
+                .catch(err => console.warn(err))
+        } else {
+            this.setState({ error: true })
+        }
+
+    }
+
+    showError = () => {
+        if (this.state.error) {
+            return <Text style={style.error}>Looks like your missing something. Please make sure you have a title, your charity/non-profit name, date, and description.</Text>
+        }
+    };
+
     getEvents = () => {
         // return (
 
@@ -46,7 +87,7 @@ export default class EventFeed extends Component {
         //     date="date"
         //     description="decription"
         //     />
-            
+
         // )
 
 
@@ -65,13 +106,14 @@ export default class EventFeed extends Component {
                     let name = event.title;
                     let date = event.date;
 
-        return ( <EventCard 
-            title={title}
-            name={name}
-            date={date}
-            description={description}
-            />)
-
+                    return (
+                        <EventCard
+                            title={title}
+                            name={name}
+                            date={date}
+                            description={description}
+                        />
+                    )
                 })
             })
             .catch(err => console.warn(err))
@@ -82,16 +124,73 @@ export default class EventFeed extends Component {
             <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
                 {/* <View style={styles.container}> */}
 
-                    {/* should we focus these on celebrities? */}
-                    <ScrollView>
-                        {this.getEvents()}
-                        <AddEventModal
-                            onPressOut={() => {
-                                this.setModalVisible(false);
-                            }}
-                            visible={this.state.modalVisible}
-                        />
-                    </ScrollView>
+                {/* should we focus these on celebrities? */}
+                <ScrollView>
+                    {this.getEvents()}
+                    <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');                  
+                    }}
+                >
+                    <View style={{ marginTop: 30, backgroundColor: Colors.blue4, flex: 1 }}>
+                        <View style={styles.modal}>
+                            <TouchableHighlight onPress={this.props.onPressOut}>
+                                <Ionicons
+                                    name={Platform.OS === 'ios' ? 'ios-close' : 'md-close'}
+                                    size={40}
+                                    color={Colors.white}
+                                    style={styles.exit}
+                                />
+                            </TouchableHighlight>
+
+
+                            {this.showError()}
+
+                            <Forminput
+                                value={this.state.title}
+                                onChangeText={(event) => this.setState({ title: event })}
+                                placeholder="Event Title"
+                                name="eventTitle"
+                                autoCorrect={true}
+                                returnKeyType="next"
+                            />
+
+                            <Forminput
+                                value={this.state.name}
+                                onChangeText={(event) => this.setState({ name: event })}
+                                placeholder="Name of Non-profit/Charity"
+                                name="charityname"
+                                autoCorrect={true}
+                                returnKeyType="next"
+                            />
+
+                            <Forminput
+                                value={this.state.date}
+                                onChangeText={(event) => this.setState({ date: event })}
+                                placeholder="Date of Event"
+                                name="eventDate"
+                                autoCorrect={true}
+                                returnKeyType="next"
+                            />
+
+                            <ForminputLong
+                                value={this.state.title}
+                                onChangeText={(event) => this.setState({ description: event })}
+                                placeholder="Event Description"
+                                name="eventDescription"
+                                autoCorrect={true}
+                                returnKeyType="done"
+                            />
+
+                            <FormButton label="Post Event" onPress={this.handleSubmit} />
+
+                        </View>
+                    </View>
+                </Modal>
+                </ScrollView>
                 {/* </View> */}
             </TouchableWithoutFeedback>
         );
@@ -121,6 +220,14 @@ const styles = StyleSheet.create({
         color: 'white',
         textAlign: "center",
         height: 20
+    },
+    modal: {
+        paddingHorizontal: 30,
+        backgroundColor: 'black'
+    },
+    exit: {
+        marginTop: 10,
+        color: 'white'
     },
     trendingButton: {
         borderRadius: 24,
